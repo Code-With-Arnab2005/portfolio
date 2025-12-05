33ec4901351e331e1e751dafdf16a2325bbffed0
Arnab Bhattacharya <arnabbhattacharya2005@gmail.com>
2025-12-05 11:51:21 +0530

    change the resume link to latest resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
                     </ul>
                 </nav>
 
-                <a target='_blank' href="https://drive.google.com/file/d/1h9Bb3ZTikA9XIishZ4y-RaWhUcjBLgQD/view?usp=sharing">
+                <a target='_blank' href="https://drive.google.com/file/d/1vLjLfQDEAXj70bY0ZaFmn6gM9efIFS0G/view?usp=sharing">
                     <button className='text-black px-4 py-3 hover:cursor-pointer rounded-lg bg-white-50'>RESUME</button>
                 </a>
                 
